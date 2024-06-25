a8e3d48e3317bb632ba018e3e5bb8c05d587090a
UndeadHedgehogTrueMeds <undeadhedgehog@proton.me>
2024-06-25 16:34:51 +0300

    wothout signs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,13 +9,7 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     //test
     //test
-<<<<<<< HEAD
     //one more test
-=======
     //test
-<<<<<<< HEAD
->>>>>>> c3a7eb3 (test UnHe)
-=======
     //test
->>>>>>> c2b25bb (test2 UnHe)
 }
