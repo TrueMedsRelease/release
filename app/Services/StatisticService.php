93c11f1c3eb281bd1b8b00b3bcd514fba441589b
truemeds-programmer <true-programmer@proton.me>
2024-10-20 14:13:12 +0300

    send checkout

--- app/Services/StatisticService.php
+++ app/Services/StatisticService.php
@@ -69,5 +69,39 @@ class StatisticService
 
     }
 
+    public static function SendCheckout()
+    {
+        $products = [];
+        $sessid = '';
+
+        foreach(session('cart') as $product)
+        {
+            $products[$product['pack_id']] = ['qty' => $product['q'], 'price' => $product['price'], 'is_ed_category' => false];
+            $sessid = !empty($product['cart_id']) ? $product['cart_id'] : '';
+        }
+
+        $products_str = base64_encode(serialize($products));
+
+        $data = [
+            'method' => 'checkout',
+            'api_key' => '7c73d5ca242607050422af5a4304ef71',
+            'product' => $products_str,
+            'ip' => request()->ip(),
+            'sessid' => $sessid,
+            'bonus' => session('cart_option.bonus_id', 0),
+            'country' => session('location.country', 'US'),
+            'total' => session('total.checkout_total', 0),
+            'customer_id' => session('form.customer_id', ''),
+            'domain_from' => request()->getHost(),
+            'fingerprint' => '',
+            'keyword' => session('keyword', ''),
+            'email' => session('form.email', ''),
+            'aff' => session('aff', 0),
+            'saff' => session('saff', ''),
+        ];
+
+        $response = Http::timeout(3)->post('http://true-services.net/checkout/order.php', $data);
+    }
+
 }
 
