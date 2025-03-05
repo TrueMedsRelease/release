ce6a00c5991928a99235b21403456467fcfbef36
Piknechek-V2 <Piknechek@proton.me>
2025-03-05 18:23:32 +0200

    fix checkup

--- resources/views/checkup.blade.php
+++ resources/views/checkup.blade.php
@@ -5,7 +5,7 @@
 @section('description', $page_properties->description)
 
 @section('content')
-<iframe width="100%" height="100%" src="https://pill-365.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000;">
+<iframe width="100%" height="100%" src="https://pill-365.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000000; top: 0; left: 0;">
 
 </iframe>
 @endsection
