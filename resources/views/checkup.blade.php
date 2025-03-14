bce306b08bb5b25dbce8dd12a4388de3abf8c3f9
Piknechek-V2 <Piknechek@proton.me>
2025-03-14 17:41:33 +0200

    change url checkup

--- resources/views/checkup.blade.php
+++ resources/views/checkup.blade.php
@@ -5,7 +5,7 @@
 @section('description', $page_properties->description)
 
 @section('content')
-<iframe width="100%" height="100%" src="https://pill-365.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000000; top: 0; left: 0;">
+<iframe width="100%" height="100%" src="https://true-checkup.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000000; top: 0; left: 0;">
 
 </iframe>
 @endsection --}}
@@ -56,7 +56,7 @@
     {!! isset($pixel) ? $pixel : '' !!}
 </head>
 <body>
-    <iframe width="100%" height="100%" src="https://pill-365.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000000; top: 0; left: 0;">
+    <iframe width="100%" height="100%" src="https://true-checkup.com/?parent_domain={{ request()->getHost() }}" style="position: fixed; z-index: 1000000; top: 0; left: 0;">
 
     </iframe>
 </body>
