9c86cf56bfc77c21fc06876231693ca610841c15
Vinnarde <vinnarde.mobivion@proton.me>
2025-10-31 18:34:34 +0300

    refactor: sessid

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -14,6 +14,14 @@ class EncryptCookies extends Middleware
     protected $except = [
         'js_stat_aff_id',
         'js_stat_design_id',
+        'js_stat_click_id',
+        'js_stat_utm_source',
+        'js_stat_utm_campaign',
+        'js_stat_utm_term',
+        'js_stat_utm_content',
+        'js_stat_sub1',
+        'js_stat_sub2',
+        'tm_network_click_id',
         'tm_session_id',
         'tm_initial_referrer',
         'tm_visit_data'
